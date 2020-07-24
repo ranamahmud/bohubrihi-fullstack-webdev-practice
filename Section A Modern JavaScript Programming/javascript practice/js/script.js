46ea8b710b42af8ee8f47ea0db0ddb1702032b9b
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-24 10:38:51 +0600

    js user input done

--- Section A Modern JavaScript Programming/javascript practice/js/script.js
+++ Section A Modern JavaScript Programming/javascript practice/js/script.js
@@ -1,3 +1,9 @@
 console.log("I'm first line");
 console.log("I'm second line");
 document.write("JS File is connected");
+
+console.log('I am "second" line');
+console.log(67.14);
+
+/*console.log("I'm first line");
+console.log("I'm second line");*/
---
+++ Section A Modern JavaScript Programming/javascript practice/js/userinput.js
@@ -0,0 +1,4 @@
+var x;
+x = prompt("Enter your name");
+console.log(x);
+document.write(x);
