803c8113e3db36275b14140a3a16520372248e1c
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-24 10:49:24 +0600

    javascrpt variable 1 done

---
+++ Section A Modern JavaScript Programming/javascript practice/js/jsvariables.js
@@ -0,0 +1,42 @@
+// var a;
+// let b;
+// a = 45;
+// b = "Rana";
+// console.log(a);
+// console.log(b);
+
+// a = 35;
+// console.log(a);
+// a = 35.24;
+// console.log(a);
+// a = "Rana";
+// console.log(a);
+
+var a = 20;
+var b = a;
+console.log(a);
+console.log(b);
+a = 30;
+console.log(a);
+console.log(b);
+
+var a = 20;
+var b = 10;
+var c = a + b;
+console.log(c);
+
+// variable naming
+var $a;
+$a = 30;
+var _abcd, abc323;
+
+var MyName,myName;
+MyName = "abcd";
+myName = "efgh";
+console.log(MyName);
+console.log(myName);
+
+var a = 20;
+console.log(a);
+var a;
+console.log(a);
