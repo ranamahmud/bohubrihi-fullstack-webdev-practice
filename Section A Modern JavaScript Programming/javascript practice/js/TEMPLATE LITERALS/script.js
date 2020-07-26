c6395c0e0bd292b3af5a27b291285925adf70d5d
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-26 09:10:28 +0600

    Template literal done

---
+++ Section A Modern JavaScript Programming/javascript practice/js/TEMPLATE LITERALS/script.js
@@ -0,0 +1,13 @@
+// Template literals (ES6)
+// Backtick ``
+let name = "Rahim";
+let age = "38";
+let dob = "21 June, 1983";
+
+console.log(`His name is ${name}
+His age is ${age}
+Date of Birth ${dob}`);
+
+let a = 34
+let b = 67
+console.log(`${a} + ${b} = ${ a + b }`);
