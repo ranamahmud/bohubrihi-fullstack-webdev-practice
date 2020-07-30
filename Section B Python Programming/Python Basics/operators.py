3338491b642281f148ca9ae786564705e9c0c27e
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-30 08:41:05 +0600

    python operators done

---
+++ Section B Python Programming/Python Basics/operators.py
@@ -0,0 +1,22 @@
+# addition
+print(5 + 2)
+# substraction
+print(5 - 2)
+# multiplication
+print(5 * 2)
+# divition
+print(5 / 2)
+# integer division
+print(5 // 2)
+# exponent
+print(5**2)
+
+# Data Types
+# integer
+# floating point data type
+# string
+print("Hello World")
+print('Python')
+print("Python" + " is" + " awesome.")
+print('python'*2)
+print(8+2.5)
