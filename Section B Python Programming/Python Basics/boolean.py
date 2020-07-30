9a37ab7eff36a9f79634f2c955743920ec5cd2e8
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-30 09:22:54 +0600

    Python boolean done

---
+++ Section B Python Programming/Python Basics/boolean.py
@@ -0,0 +1,52 @@
+# Boolean Data Types has only two values
+# True
+# False
+
+# comparision operator
+# equality ==
+# not equal != 
+# greater than >
+# less than <
+# greater than equal >=
+# less than equal <=
+
+print(10 == 10)
+print(10!=6)
+print(10!=10)
+print(42 <= 422)
+print(32<10)
+print(32>10)
+
+x = 1000
+print(x < 100)
+print(x <= 1000)
+
+print(True == True)
+print(False != True)
+print(False == False)
+print(False != False)
+print(43== 43.0)
+print(42 == '42')
+
+# Binary Boolean Operator
+# and
+# or
+# not
+
+print(4 < 5 and 9 < 6)
+print(1==2 and 3 == 4)
+print(1==1 and 2 == 2)
+print(1==2 or 5==5)
+print(1==2 or 2==3)
+print(not 10==10)
+print(not 10!=100)
+print(not True)
+print(not False);
+print(not not not not False)
+
+print(True or True)
+print(True or False)
+print(True and True)
+print(True and False)
+
+print(10 + 10 == 20 and not 10+10==10 and 2*10 == 10+10)
---
+++ Section B Python Programming/Python Basics/variables.py
@@ -0,0 +1,24 @@
+a = 2
+print(a)
+b = 2.2
+print(b)
+x = 'string'
+print(x)
+# overriding
+a = 10
+a = 500
+print(a)
+b = 'x'
+a = b
+print(a)
+
+# rules for naming variables
+# no number to begin with
+# python keywords not allowed
+# can contain letters,numbers and underscores
+
+tasty_chocolae = 800
+# 90=0
+
+# 9io = 90
+# True = 89
