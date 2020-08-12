7c1e9df51f413e4fe10c6cf1e7dfe2172c2d1bb1
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-13 02:56:30 +0600

    Array Destructuring  done

---
+++ Section A Modern JavaScript Programming/LEVELING UP TO ES6/Array Destructuring.js
@@ -0,0 +1,10 @@
+// Array Destructuring
+
+let fruits = ["Apple", "Grape", "Orange"];
+
+// let [fruit1, fruit2, fruit3] = fruits;
+
+let [fruit1, , fruit3] = fruits;
+console.log(fruit1);
+// console.log(fruit2);
+console.log(fruit3);
