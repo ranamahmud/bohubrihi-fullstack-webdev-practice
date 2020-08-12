5d8e471334bcd332cc94da5e0d5d9eb4d53a0eaa
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-13 02:48:55 +0600

    template literal done

---
+++ Section A Modern JavaScript Programming/LEVELING UP TO ES6/Template Literals.js
@@ -0,0 +1,18 @@
+console.log("First Line");
+console.log("Second Line");
+
+console.log(`First Line
+Second Line`);
+
+let name = "Rahim";
+let age = "38";
+let dob = "21 June, 1983";
+
+console.log(`His name is ${name}
+His age is ${age}
+Date of Birth ${dob}`);
+
+let a = 34;
+let b = 67;
+
+console.log(`${a} + ${b} = ${a + b}`);
