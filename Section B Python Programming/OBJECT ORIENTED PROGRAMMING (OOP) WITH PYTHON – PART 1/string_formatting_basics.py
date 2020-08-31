f8a928ef336cfb3a9e584f78c907b92aca77012d
Md Rana Mahmud <ranamd0@gmail.com>
2020-09-01 00:25:24 +0600

    Python String Fromatting Basics  done

---
+++ Section B Python Programming/OBJECT ORIENTED PROGRAMMING (OOP) WITH PYTHON – PART 1/string_formatting_basics.py
@@ -0,0 +1,17 @@
+print('I am %s' %'Rana')
+print('I am %s and I am %d years old'%('akil',23))
+
+name = 'mark'
+age = 25
+profession = 'Player'
+earning = 245000.3444
+
+print('I am %s and I am %d years old. I am a %s and I earn %.2f$'%(name,age,profession,earning))
+
+print('I am {} and I am {}'.format('akil','happy'))
+
+print('I am {1} and I am {0}'.format(name,'happy'))
+
+food = 'sandwitch'
+language = 'python'
+print(f'I am {name} and I eat{food}. I don\'t believe {language} haters\n And {2**2}={4}')
