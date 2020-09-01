c65ffdc7d9932630a227fef269210db40d826942
Md Rana Mahmud <ranamd0@gmail.com>
2020-09-01 20:37:28 +0600

    first react app done

---
+++ .history/Section F React Native for Multiplatform App Development/Intro and Setup/HelloWorld/App_20200901203353.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  const [value, onChangeText] = React.useState('Useless Placeholder');
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      <Text>My First React Native App</Text>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => handleInputChange(text)}
+      value={value}
+    />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ .history/Section F React Native for Multiplatform App Development/Intro and Setup/HelloWorld/App_20200901203429.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  const  handleInputChange = text =>{
+    console.log(text);
+  }
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      <Text>My First React Native App</Text>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => handleInputChange(text)}
+      value={value}
+    />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
