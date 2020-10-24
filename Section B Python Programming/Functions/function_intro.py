485fcdc76b8089829442404db4e20c668e109418
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-24 12:31:03 +0600

    list intro done

---
+++ Section B Python Programming/Functions/function_intro.py
@@ -0,0 +1,16 @@
+def f():
+    print("hello")
+f()
+
+def sum(a, b):
+    return a + b
+
+a = sum(1,2)
+print(a)
+
+def print_my_name(name):
+    print("Your name is "+name)
+print_my_name("Rana")
+
+var = print('ok')
+print(type(var))
---
+++ Section B Python Programming/Functions/lists_intro.py
@@ -0,0 +1,28 @@
+a = [1, 2, 3]
+print(a)
+b = ['a','b','c']
+print(b)
+
+z = [3.1415,'pi',23]
+print(z)
+
+print(a[0])
+
+# nested list
+
+x = [[1,2,3],[4,5,6]]
+print(x)
+print(x[0][1])
+
+
+# negative indexes
+p = [1,2,3,4,5,6]
+print(p[-1])
+print(p[-2])
+
+# list slicing
+q = [1,2,3,4,5,6]
+print(q[0:5])
+print(q[1:3])
+print(q[0:-2])
+print(q[1:-1])
