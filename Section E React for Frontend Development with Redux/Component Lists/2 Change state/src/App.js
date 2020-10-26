029756ae1d359d39a63362486fe3b213c0fed35d
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-26 11:18:38 +0600

    change state done

--- Section E React for Frontend Development with Redux/Component Lists/2 Change state/src/App.js
+++ Section E React for Frontend Development with Redux/Component Lists/2 Change state/src/App.js
@@ -4,28 +4,15 @@ import Book from './components/Book';
 
 
 class App extends Component {
+
   state = {
     books: [
-      { bookName: '1984', writer: 'George Orwell' },
-      { bookName: 'The vinci code', writer: 'Dan Brown' },
-      { bookName: 'The Alchemist', writer: 'Paulo Coelho' },
+      { id: 1, bookName: '1984', writer: 'George Orwell' },
+      { id: 2, bookName: 'The vinci code', writer: 'Dan Brown' },
+      { id: 3, bookName: 'The Alchemist', writer: 'Paulo Coelho' },
     ],
   }
 
-  changeWithInputState = e => {
-    this.setState({
-      books: [
-        { bookName: e.target.value, writer: 'George Orwell' },
-        { bookName: 'The vinci code', writer: 'Dan Brown' },
-        { bookName: 'The Alchemist', writer: 'Paulo Coelho' },
-      ],
-    })
-  }
-  deleteBookState = index => {
-    const books = this.state.books;
-    books.splice(index, 1)
-    this.setState({ books: books })
-  }
   render() {
     const style = {
       border: '1px solid red',
@@ -33,12 +20,28 @@ class App extends Component {
       backgroundColor: 'black',
       color: "white"
     }
+    const deleteBookState = index => {
+      const books = this.state.books;
+      books.splice(index, 1)
+      this.setState({ books: books })
+    };
+    const
+      changeWithInputState = (e, index) => {
+        const book = { ...this.state.books[index] }
+        book.bookName = e.target.value
+        const books = [...this.state.books]
+        books[index] = book
+        this.setState({ books: books })
+      }
+
     const books = this.state.books
       .map(function (book, index) {
         return (
-          <Book key={index} bookName={book.bookName}
+          <Book key={book.id}
+            inputName={(event) => changeWithInputState(event, index)}
+            bookName={book.bookName}
             writer={book.writer}
-            delete={() => this.deleteBookState(index)}
+            delete={() => deleteBookState(index)}
           />
         )
 
@@ -46,8 +49,7 @@ class App extends Component {
     return (
       <div className="App" >
         <h1 style={style}>Book List</h1>
-        <button onClick={() => this.changeBookState('Nineteen Eighty Four')}>Change State</button>
-        <input type="text" name="" onChange={this.changeWithInputState} id="" />
+
         {
           books
         }
